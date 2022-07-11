f1a5c894821c91181b6104af1e3c3437b2e5d7c3
Von <107226923+ArzuVon@users.noreply.github.com>
2022-07-11 16:51:18 -0400

    added hello world server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-require("dotenv").config(); //require here imports from package.json
+require('dotenv').config(); //require here imports from package.json
 const port = process.env.PORT ?? 3000;
 
 const server = require('./server.js'); //require imports the file from somewhere else
--- server.js
+++ server.js
@@ -3,13 +3,19 @@
 const express = require('express');
 
 const hello = (req, res) => {
-    res.status(200).send("Hello, World");
-}
+    res.status(200).send('Hello, World'); //this handle app.get invokes the hello ad the / gets called and it sends hback a 200 okay and then sends the hello world
+};
 
 const app = express();
 
-app.get('/', hello);
+app.get('/', hello); //handle happens at the end and gives data back. Endpoint to port 3000
+
+// ---
 
 function start(port){
     app.listen(port, () => console.log(`Server listening on port ${port}`));
 }
+
+module.exports = {
+    start,
+};
