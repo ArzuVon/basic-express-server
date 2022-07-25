9abf66c383cf17980c2c9db92e38e9bab823be85
Von <107226923+ArzuVon@users.noreply.github.com>
2022-07-25 07:23:32 -0400

    fixed path in server.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 require("dotenv").config(); // loads anything from env file && require here imports from package.json
 const port = process.env.PORT ?? 3000;
 
-const server = require("./server.js"); //require imports the file from somewhere else
+const server = require("./src/server"); //require imports the file from somewhere else
 server.start(port);
