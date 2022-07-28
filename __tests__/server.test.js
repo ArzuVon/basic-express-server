9e12d97151099e37d65911cf12ae9c2777bcfcc6
Von <107226923+ArzuVon@users.noreply.github.com>
2022-07-27 23:23:11 -0400

    CRUD for boxer model complete

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -10,38 +10,105 @@
 
 //going to have number of nested describes
 const supertest = require('supertest');
+const { db } = require('../src/db');
 const server = require('../server.js');
 
 const request = supertest(server.app);
 
+//clears the database...
 
-describe('Node Server', () => { //description of what we are trying to test
-  it('says hello world', async() =>{ //a single test case
-    //set up test so it can do a thing
-    //prepare the server
-    //(see above)
+// describe('Node Server', () => { //description of what we are trying to test
+//   it('says hello world', async() =>{ //a single test case
+//set up test so it can do a thing
+//prepare the server
+//(see above)
 
-    //perform an action, that does the thing
-    //request the /route
-    const response = await request.get('/'); //the request is a promise
-
-    //assert or expect the result of the action
-    // expect the / route to respond with hello, the thing you want to look at
-    expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello, World');
-
+describe('models', () => {
+  beforeEach(async () =>{
+    await db.sync();
   });
 
-  //test driven development when you write the test first
-  it('returns some data', async () => {
-    const response = await request.get('/data');
+  //Creates Boxer
 
+  it('creates a boxer', async () => {
+    let response = await request.post('/boxer').send({
+      boxerType: 'Test boxer',
+      fightStyle: 'string',
+    });
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({
-      name: 'Von',
-      role: 'Developer', //in Test Driven Developemnt write test first than minimum amount of code
+    expect(response.body).toMatchObject({
+      boxerType: 'Test boxer',
+      fightStyle: 'string', //in Test Driven Developemnt write test first than minimum amount of code
     });
   });
+
+  //Grabs or post a boxer
+  it('retrieves a boxer', async () => {
+    let createResponse = await request.post('/boxer').send({
+      boxerType: 'Test boxer',
+      fightStyle: 'string',
+    });
+
+    expect(createResponse.status).toBe(200);
+    const createdId = createResponse.body.id;
+
+    let retrieveResponse = await request.get(`/boxer/${createdId}`);
+
+    expect(retrieveResponse.status).toBe(200);
+    expect(retrieveResponse.body).toMatchObject({
+      boxerType: 'Test boxer',
+      fightStyle: 'string',
+    });
+  });
+
+  //it Lists Boxer
+  it('can list a boxer', async () =>{
+    let listBoxerRes = await request.get('/boxer');
+    expect(listBoxerRes.status).toBe(200);
+    expect(listBoxerRes.body[0]).toHaveProperty('boxerType');
+  });
+
+  //it deleted boxer
+  it('deletes a boxer', async() => {
+    let createResponse = await request.post('/boxer').send({
+      boxerType: 'Test boxer',
+      fightStyle: 'string',
+    });
+
+    expect(createResponse.status).toBe(200);
+    const createdId = createResponse.body.id;
+
+    let retrieveResponse = await request.delete(`/boxer/${createdId}`);
+    expect(retrieveResponse.status).toBe(200);
+
+  });
+
+  //it Updates boxer
+  it.skip('can update a boxer', async ()=>{
+    let createResponse = await request.post('/boxer').send({
+      boxerType: 'Test boxer',
+      fightStyle: 'string',
+    });
+
+    expect(createResponse.status).toBe(200);
+    const createdId = createResponse.body.id;
+
+    const updateRes = await request.put(`/boxer/${createdId}`).send({ boxerType: 'light weight'});
+    expect(updateRes.status).toBe(200);
+
+  });
+
+
 });
 
 
+//   //test driven development when you write the test first
+//   it('returns some data', async () => {
+//     const response = await request.get('/data');
+
+//     expect(response.status).toBe(200);
+//     expect(response.body).toEqual({
+//       name: 'Von',
+//       role: 'Developer',
+//     });
+//   });
